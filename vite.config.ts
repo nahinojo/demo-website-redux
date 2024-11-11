7ebd579a6d26996316793467d384788082e553ad
Noah Hinojos <noah.hinojos@gmail.com>
2024-11-11 15:31:47 -0800

    chore: noted config for github pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // For github pages, must be the name of the repo.
   base: '/demo-website-redux/.'
 })
