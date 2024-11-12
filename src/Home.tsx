12d51ccd2b1da0121c3b640584b6eb75ce56f9b6
Noah Hinojos <noah.hinojos@gmail.com>
2024-11-11 16:46:15 -0800

    feat: setup fonts and home header

---
+++ src/Home.tsx
@@ -0,0 +1,12 @@
+import './stylesheets/global.css'
+import Header from './components/Header'
+
+function Home() {
+  return (
+    <>
+      <Header />
+    </>
+  )
+}
+
+export default Home
---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,18 @@
+import './Header.css'
+
+const Header = () => {
+  return (    <>
+    <header>
+      <div id='header-container'>
+        <div id='uci-department'>
+          <h1 id='uci' >UCI</h1>
+          <h2 id='department'>Department of Physics & Astronomy</h2>
+        </div>
+        <h3 id='program'>Lecture Demonstration Program</h3>
+      </div>
+    </header>
+  </>
+  )
+}
+
+export default Header
